cd5583ab6a77cffcfae776ecd84670684b52786f
Raimundo82 <joaoluisraimundo@tecnico.ulisboa.pt>
2022-08-09 23:57:06 +0100

    deploy

--- dist/assets/licencaParentalTemplate.699c0592.js
+++ dist/assets/licencaParentalTemplate.699c0592.js
@@ -1,4 +1,4 @@
-import{b as d}from"./index.5780bbb3.js";const a=(o,e)=>({submissionContent:{header:`Exmo. Senhor
+import{b as d}from"./index.649715e5.js";const a=(o,e)=>({submissionContent:{header:`Exmo. Senhor
 Vice-almirante Superintendente do Pessoal`,body:`${o.idNumber} ${o.category} ${o.completeName}, do mapa de pessoal civil da Marinha, a prestar servi\xE7o na ${o.unit} vem requerer a V. Ex\xAA., que lhe seja concedida ${e.licence.toLowerCase()} no per\xEDodo de ${e.dateStart} a ${e.dateEnd} nos termos do do n.\xBA/al\xEDnea ${e.number} do artigo ${e.article}.\xBA, do C\xF3digo do Trabalho, aprovado pela Lei n.\xBA 7/2009, de 12 de fevereiro, alterado pelo artigo 2.\xBA da Lei n.\xBA 120/2015 de 1 de setembro,`,footer:`Pede Deferimento
 
 ${d()}`}});export{a as submissionContent};
