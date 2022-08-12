4caf5dd246f4cb33cb85c5632b800e73f1715206
Raimundo82 <joaoluisraimundo@tecnico.ulisboa.pt>
2022-08-12 14:33:21 +0100

    deploy

--- assets/requirements.12bbd1e7.js
+++
@@ -1,8 +0,0 @@
-import{g as o,c,a,_ as i}from"./index.84b228c9.js";import"./bootstrap.esm.min.9853106f.js";const s=(e,t)=>{const n=e[t];return n?typeof n=="function"?n():Promise.resolve(n):new Promise((_,r)=>{(typeof queueMicrotask=="function"?queueMicrotask:setTimeout)(r.bind(null,new Error("Unknown variable dynamic import: "+t)))})},l="Dados do requerimento",m={title:l};async function u(e){const t=(await s(Object.assign({"./licencaParental/licencaParental.js":()=>i(()=>import("./licencaParental.dd705dfa.js"),["assets/licencaParental.dd705dfa.js","assets/index.84b228c9.js","assets/index.b0875c02.css","assets/licencaParentalTemplate.62b5337f.js"]),"./licencaParental/licencaParentalTemplate.js":()=>i(()=>import("./licencaParentalTemplate.62b5337f.js"),["assets/licencaParentalTemplate.62b5337f.js","assets/index.84b228c9.js","assets/index.b0875c02.css"]),"./licencaSemRemuneracap/licencaSemRemuneracao.js":()=>i(()=>import("./licencaSemRemuneracao.f5fd7b8c.js"),[])}),`./${e}/${e}.js`)).default;t()}const q=({requirementName:e})=>{o("div#app").appendChild(c(d())),u(e)},d=()=>html`
-    <style id="requirementStyle"></style>
-    <h4 class="text-center mb-5">
-      ${m.title}
-      (${a().requirementsData.find(e=>e.value===a().identificationData.selectedRequirement).name})
-    </h4>
-    <div id="requirementContainer"></div>
-  `;export{q as requirement};
